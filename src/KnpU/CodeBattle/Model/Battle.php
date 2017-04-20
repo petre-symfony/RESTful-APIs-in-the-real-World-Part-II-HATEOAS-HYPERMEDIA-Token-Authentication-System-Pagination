5d766e2d53bf95114eae50f0e1157cf4cc4c0919
petrero <petrero@gmail.com>
2017-04-20 19:07:37 +0300

    Copy start code from knp site

---
+++ src/KnpU/CodeBattle/Model/Battle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace KnpU\CodeBattle\Model;
+
+class Battle {
+  /* All public properties are persisted */
+  public $id;
+
+  /**
+   * @var Programmer
+   */
+  public $programmer;
+
+  /**
+   * @var Project
+   */
+  public $project;
+
+  public $didProgrammerWin;
+
+  public $foughtAt;
+
+  public $notes;
+}
---
+++ src/KnpU/CodeBattle/Model/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace KnpU\CodeBattle\Model;
+
+class Project {
+  /* All public properties are persisted */
+  public $id;
+
+  public $name;
+
+  /**
+   * 1-10 difficulty level of the project
+   *
+   * @var integer
+   */
+  public $difficultyLevel;
+}
