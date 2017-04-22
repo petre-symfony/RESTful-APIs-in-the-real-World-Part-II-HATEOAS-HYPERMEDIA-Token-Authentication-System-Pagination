73b683b8bf30925e8cbea3d10b35ff4ac585b5ac
petrero <petrero@gmail.com>
2017-04-22 17:44:28 +0300

    Creating the Token Resource
    So let’s do as little work as possible to get this going. The first thing to know is that we do have a token table. And just like with our other tables like the programmer table where we have a Programmer class, I’ve also created an ApiToken class:
    // src/KnpU/CodeBattle/Security/Token/ApiToken.php
    namespace KnpU\CodeBattle\Security\Token;
    
    use Symfony\Component\Validator\Constraints as Assert;
    
    class ApiToken {
      public {dollar_sign}id;
    
      public {dollar_sign}token;
    
      /**
       * @Assert\NotBlank(message='Please add some notes about this token')
       */
      public {dollar_sign}notes;
    
      public {dollar_sign}userId;
    
      public {dollar_sign}createdAt;
    
      public function __construct({dollar_sign}userId) {
        {dollar_sign}this->userId = {dollar_sign}userId;
        {dollar_sign}this->createdAt = new \DateTime();
        {dollar_sign}this->token = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
      }
    }
    
    If we can create this new ApiToken object, then we can use some ORM-magic I setup to save a new row to that table.
    
    So let’s start doing that: {dollar_sign}token = new ApiToken();. I’ll add the use statement for that.
    
    You can see immediately it’s angry with me because I need to pass the userId to the constructor. Now, what is the id of the current user? Remember, in our scenario, we’re passing HTTP Basic authentication. So here we need to grab the HTTP Basic username and look that up in the database. I’m not going to worry about checking the password yet, we’ll do that in a second.
    
    In Silex, whenever you need request information you can just type hint a  variable in your controller and it will be passed in. Am I sounding like a broken record yet? Don’t forget your use statement!
    
    You may or may not remember this - I had to look it up - but if you want to get the HTTP Basic username that’s sent with the request, you can say {dollar_sign}request->headers->get('PHP_AUTH_USER'). Oops don’t forget your equals sign. Next I’ll look this up in our user table. For now we’ll just assume it exists: I’m living on the edge by not doing any error handling. And then, we’re going to say {dollar_sign}user->id
    
    Perfect!

--- src/KnpU/CodeBattle/Controller/Api/TokenController.php
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -2,14 +2,19 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Security\Token\ApiToken;
 use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
 
 class TokenController extends BaseController{
   protected function addRoutes(\Silex\ControllerCollection $controllers) {
     $controllers->post('/api/tokens', array($this, 'newAction'));
   }
   
-  public function newAction(){
-    return 'FOO';
+  public function newAction(Request $request){
+    $username = $request->headers->get('PHP_AUTH_USER');
+    $user = $this->getUserRepository()->findUserByUsername($username);
+    
+    $token = new ApiToken($user->id);
   }
 }
