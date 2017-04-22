8d9922d2ea484c7116dc80f626177a83ad8c97c7
petrero <petrero@gmail.com>
2017-04-22 17:30:04 +0300

    Creating the TokenController
    To get this working, I’m going to create an entirely new controller class and make it look a bit like my ProgrammerController. Make it extend the BaseController class which we’ve been adding more helper methods into. Notice that I did just add a use statement for that.
    
    And it expects us to have one method called addRoutes. This is special to my implementation of Silex, but you’ll remember that we have this at the top of ProgrammerController and that’s just where we build all of our endpoints. We can do the same things here. We’ll add a new POST endpoint for /api/tokens that will execute a method called newAction when we hit it.
    
    So let’s go back and rerun the tests. Look at that, it is working. The test still fails, but instead of a 404, we see a 200 status code because we’re returning foo.

---
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -0,0 +1,15 @@
+<?php
+namespace KnpU\CodeBattle\Controller\Api;
+
+use KnpU\CodeBattle\Controller\BaseController;
+use Silex\ControllerCollection;
+
+class TokenController extends BaseController{
+  protected function addRoutes(\Silex\ControllerCollection $controllers) {
+    $controllers->post('/api/tokens', array($this, 'newAction'));
+  }
+  
+  public function newAction(){
+    return 'FOO';
+  }
+}
